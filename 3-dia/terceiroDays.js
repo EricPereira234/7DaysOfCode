f560f55d361d2eb101da858c432e468fd491797f
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-08-20 10:39:54 -0300

    Upteste3

--- 3-dia/terceiroDays.js
+++ 3-dia/terceiroDays.js
@@ -1,8 +1,8 @@
 
 
 function Resposta() {
-
-
+    //variáveis do sistema!
+    let name = prompt('Qual o seu nome ?');
     let pergunta1 =
         prompt(`você quer seguir para área de Front-End ou seguir para a
     área de Back-End ?`);
@@ -12,13 +12,14 @@ function Resposta() {
     let pergunta5;
     let pergunta6;
     let contador = 0;
+    //fim das variáveis !
 
     if (pergunta1 == 'Front-End') {
-        pergunta2 = prompt(`quer aprender React ou aprender Vue`)
+        pergunta2 = prompt(`quer aprender React ou aprender Vue ?`)
         pergunta3 = prompt(`você deseja se especializar nesta área ou 
         continuar evoluindo para ser um Fullstack ?`)
     } if (pergunta1 == 'Back-End') {
-        pergunta2 = prompt(`você poderá aprender C# ou aprender Java`)
+        pergunta2 = prompt(`você poderá aprender C# ou aprender Java !`)
     }
 
     //repetição de um bloco de perguntas
@@ -35,8 +36,9 @@ function Resposta() {
 
     //aqui exibe a mensagem final para o usuário
     let resposta = window.document.getElementById('container')
-    resposta.innerHTML = `Olá você escolheu ser ${pergunta1},
-    e você quer aprender ${pergunta2}, 
-    e você pretende ainda aprender ${pergunta4}, ${pergunta5}, ${pergunta6}, parabéns continui assim!
+    resposta.innerHTML = `Olá <b>${name}</b> ! <br/> você escolheu ser ${pergunta1},
+    e você quer aprender ${pergunta2}, <br/>
+    e você pretende ainda aprender ${pergunta4}, ${pergunta5}, ${pergunta6}.<br/>
+     <b> parabéns continue assim!</b>
     `
 }
